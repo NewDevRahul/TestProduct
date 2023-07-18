8a4bf7b649297d3240f9d4501ed562cab1ca70fc
RoshanGheenaiaya <roshan.gheenaiya4039@gmail.com>
2023-07-18 17:19:42 +0530

    AddPolicy

--- Program.cs
+++ Program.cs
@@ -16,6 +16,11 @@ builder.Services.AddScoped<IUserRepository,UserRepository>();
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>{
+        options.AddPolicy(name: "AllowOrigin", builder =>{
+                builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
